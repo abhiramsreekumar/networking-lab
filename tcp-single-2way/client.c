b566d2c90740cd182659f37b32662976ccdef6e3
Abhiram Sreekumar <abhiramsreekumar314@gmail.com>
2022-09-13 09:28:33 +0000

    modified:   tcp-single-2way/client.c

--- tcp-single-2way/client.c
+++ tcp-single-2way/client.c
@@ -3,28 +3,23 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#define CHECK(expr)                                                            \
-    if ((expr) < 0)                                                            \
-        return -1;                                                             \
-    else                                                                       \
-        printf("%s;\n", #expr);
 
 int main(void) {
     int sockfd;
     struct sockaddr_in saddr;
     char buf[2000] = {'\0'};
-    CHECK(sockfd = socket(AF_INET, SOCK_STREAM, 0))
+    sockfd = socket(AF_INET, SOCK_STREAM, 0)
     saddr.sin_family = AF_INET;
     saddr.sin_port = htons(3333);
     saddr.sin_addr.s_addr = INADDR_ANY;
-    CHECK(connect(sockfd, (struct sockaddr *)&saddr, sizeof(saddr)))
+    connect(sockfd, (struct sockaddr *)&saddr, sizeof(saddr))
     for (;;) {
         bzero(buf, sizeof(buf));
         scanf("%s", buf);
         if (!strcmp(buf, "x"))
             break;
-        CHECK(send(sockfd, buf, sizeof(buf), 0))
-        CHECK(recv(sockfd, buf, sizeof(buf), 0))
+        send(sockfd, buf, sizeof(buf), 0)
+        recv(sockfd, buf, sizeof(buf), 0)
         printf("SERVER: %s\n", buf);
     }
     close(sockfd);
