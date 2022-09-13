27a60f4565221d101412e11c15de97a8b02f2325
Abhiram Sreekumar <abhiramsreekumar314@gmail.com>
2022-09-13 09:37:01 +0000

    modified:   tcp-single-2way/client.c

--- tcp-single-2way/client.c
+++ tcp-single-2way/client.c
@@ -8,18 +8,18 @@ int main(void) {
     int sockfd;
     struct sockaddr_in saddr;
     char buf[2000] = {'\0'};
-    sockfd = socket(AF_INET, SOCK_STREAM, 0)
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
     saddr.sin_family = AF_INET;
     saddr.sin_port = htons(3333);
     saddr.sin_addr.s_addr = INADDR_ANY;
-    connect(sockfd, (struct sockaddr *)&saddr, sizeof(saddr))
+    connect(sockfd, (struct sockaddr *)&saddr, sizeof(saddr));
     for (;;) {
         bzero(buf, sizeof(buf));
         scanf("%s", buf);
         if (!strcmp(buf, "x"))
             break;
-        send(sockfd, buf, sizeof(buf), 0)
-        recv(sockfd, buf, sizeof(buf), 0)
+        send(sockfd, buf, sizeof(buf), 0);
+        recv(sockfd, buf, sizeof(buf), 0);
         printf("SERVER: %s\n", buf);
     }
     close(sockfd);
