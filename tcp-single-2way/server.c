b71b5a26b185cb97088e52f4e8e70aa0496c2dd9
Abhiram Sreekumar <abhiramsreekumar314@gmail.com>
2022-09-13 09:35:32 +0000

    modified:   tcp-single-2way/server.c

--- tcp-single-2way/server.c
+++ tcp-single-2way/server.c
@@ -3,28 +3,29 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+
+
 int main(void) {
     int sockfd, csock;
     struct sockaddr_in saddr, caddr;
     socklen_t caddrsz = sizeof(caddr);
     char buf[2000] = {'\0'};
-    sockfd = socket(AF_INET, SOCK_STREAM, 0)
-    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int));
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
     saddr.sin_family = AF_INET;
     saddr.sin_port = htons(3333);
     saddr.sin_addr.s_addr = INADDR_ANY;
-    bind(sockfd, (struct sockaddr *)&saddr, sizeof(saddr))
-    listen(sockfd, 1)
-    csock = accept(sockfd, (struct sockaddr *)&caddr, &caddrsz)
+    bind(sockfd, (struct sockaddr *)&saddr, sizeof(saddr));
+    listen(sockfd, 1);
+    csock = accept(sockfd, (struct sockaddr *)&caddr, &caddrsz);
     for (;;) {
         bzero(buf, sizeof(buf));
-        recv(csock, buf, sizeof(buf), 0)
+        recv(csock, buf, sizeof(buf), 0);
         printf("CLIENT: %s\n", buf);
         bzero(buf, sizeof(buf));
         scanf("%s", buf);
         if (!strcmp(buf, "x"))
             break;
-        send(csock, buf, sizeof(buf), 0)
+        send(csock, buf, sizeof(buf), 0);
     }
     close(csock);
     close(sockfd);
